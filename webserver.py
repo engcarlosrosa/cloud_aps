7de84e87bdd09dd6d0a269662ccf5d3b1cea4bd5
engcarlosrosa <kdu.carloseduardo@gmail.com>
2018-11-30 18:11:29 -0200

    Bugs da APS2 consertados

--- webserver.py
+++ webserver.py
@@ -1,8 +1,30 @@
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, request, render_template, jsonify
 import requests
 import os.path
-from aps_1 import *
+#from aps_1 import *
 
+dicionario_tarefas = {}
+indice = 1
+
+class Tarefa(object):
+	"""docstring for Tarefa"""
+	def __init__(self, atributo_1, atributo_2):
+		
+		self.atributo_1 = atributo_1
+		self.atributo_2 = atributo_2
+
+	def muda_atributo_1(self, novo_atributo_1):
+		
+		self.atributo_1 = novo_atributo_1
+
+	def muda_atributo_2(self, novo_atributo_2):
+
+		self.atributo_2 = novo_atributo_2
+
+	def serialize(self):
+		return {'atributo_1': self.atributo_1, 'atributo_2': self.atributo_2}
+
+	
 def diretorio():
 	return os.path.abspath(os.path.dirname(__file__))
 
@@ -27,27 +49,42 @@ def indice():
 
 @app.route("/Tarefa/", methods=['POST', 'GET'])
 def tarefas():
+	print(request.method)
 	if request.method == 'POST':
+		
+		dado = request.json
+
 		try:
-			dado = request.get_json()
-			valor = dado.split(":")[1]
-			cria_tarefa(valor)
-			return "Tarefa/POST"
+			dado = request.json
+			print(dado)
+			valor = Tarefa(dado["a1"], dado["a2"])
+
+			print(len(dicionario_tarefas))
+
+			if len(dicionario_tarefas) == 0:
+				indice = 1
+			else:
+				indice = len(dicionario_tarefas) + 1
+
+
+			print(dicionario_tarefas)
+			print(valor)
+
+			dicionario_tarefas[str(indice)] = valor
+
+			print(dicionario_tarefas)
+
+			return jsonify(dado), 200
 		except:
 			print("POST ERROR")
 
-		else:
-			try:
-				lista = []
-
-				for chave, valor in dicionario_tarefas.items():
-					lista.append(valor)
-
-				print(lista)
-				return "Tarefa/GET"
-
-			except:
-				return "Tarefa/GET ERROR"
+	elif request.method == 'GET':
+		print("A")
+		print(dicionario_tarefas)
+		#return {'tarefas': [marshal(task, task_fields) for task in dicionario_tarefas]}, 200
+		return jsonify(dicionario_tarefas=[v.serialize() for k,v in dicionario_tarefas.items()]), 200
+		#return jsonify({ 'tarefas' : dicionario_tarefas }), 200
+		#return jsonify([marshal(e, tarefas_tipos) for e in dicionario_tarefas]), 200
 
 @app.route("/Tarefa/<int:id_>",methods=['GET', 'PUT', 'DELETE'])
 def atualiza(id_):
@@ -57,6 +94,7 @@ def atualiza(id_):
 
 	elif request.method == 'PUT':
 		valor = str(request.get_data())
+		print(valor)
 		valor = valor.split("=")[1]
 		dicionario_tarefas[str(id_)] = valor
 		return "Tarefa/id/PUT"
@@ -70,4 +108,4 @@ def healthcheck():
 	return "200"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
